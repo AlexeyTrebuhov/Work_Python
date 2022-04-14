9efc30c93a73bd97860064bf6371c48e2fa5c907
Alex_T <saler@inbox.ru>
2022-04-14 21:58:25 +0300

    2

---
+++ Work_Python/2/2.py
@@ -0,0 +1,13 @@
+import os
+os.system("cls")
+
+number=float(input('Введите число '))
+number=str(float(number))
+number2=number.split('.')
+print(number2[0])
+print(number2[1])
+number3=number2[1]
+print(number3[0])
+#number3=number-number2
+#number4=number3
+#print (number4[0])
