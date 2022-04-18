dbab353158a5961cb4f168c63372c5caf6608ae2
Alex_T <saler@inbox.ru>
2022-04-18 20:56:24 +0300

    Задача 2

--- Work_Python/2/2.py
+++ Work_Python/2/2.py
@@ -1,13 +1,11 @@
+# 2.	Найти максимальное из пяти чисел
+
 import os
 os.system("cls")
 
-number=float(input('Введите число '))
-number=str(float(number))
-number2=number.split('.')
-print(number2[0])
-print(number2[1])
-number3=number2[1]
-print(number3[0])
-#number3=number-number2
-#number4=number3
-#print (number4[0])
+list=[-5,89,37,110,5]
+temp=0
+for i in list:
+    if i>temp:
+       temp=i
+print(temp)
