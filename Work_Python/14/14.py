c7da56e586ff37a58ec33e79b321067bd52c2dba
Alex_T <saler@inbox.ru>
2022-04-22 07:06:26 +0300

    Задача 14-2

--- Work_Python/14/14.py
+++ Work_Python/14/14.py
@@ -11,6 +11,10 @@ b2 = b1.split('.')
 b3 = b2[0]
 b4 = b2[1]
 
+b5 = b3.split('-')
+b6 = b5[0]
+b7 = b5[1]
+
 print (b3)
 print (b4)
 
@@ -18,7 +22,16 @@ sum = 0
 for i in b4:
     temp = int(i)
     sum = sum + temp
+for j in b7:
+    temp = int(j)
+    sum = sum + temp
+
 print (sum)
+
+z = chr(b[0])
+print (z)
+ 
+
 
 
 #res = [int(x) for x in str(b2[0])]
