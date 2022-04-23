34dc68cb64313a4c625bed0eb388bd907d88a02b
Alex_T <saler@inbox.ru>
2022-04-23 23:23:28 +0300

    Задача 17-1

---
+++ Work_Python/17/17.py
@@ -0,0 +1,19 @@
+# 17.	Задать список из N элементов, заполненных числами из [-N, N]. 
+# Найти произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число
+
+import random
+import os
+os.system ("cls")
+
+n = int ( input( 'Введите число N \n'))
+
+
+mas = []
+for i in range(n):
+   mas.append(random.randint(-n, n))
+print(mas)
+
+f = open(D:\Обучение\Работа в Git\Work_Python\17\file.txt)
+for line in f:
+    line
+
