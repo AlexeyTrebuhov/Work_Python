d76b0e25992838552f1fdac10aec943e2ca5bb52
Alex_T <saler@inbox.ru>
2022-04-25 01:08:18 +0300

    Задача 17-4

--- Work_Python/17/17.py
+++ Work_Python/17/17.py
@@ -12,8 +12,25 @@ for i in range(n):
    mas.append(random.randint(-n, n))
 print(mas)
 
-path = 'file.txt'
-data = open(path, 'r')
-for line in data:
-      print (line)
-data.close()
+m = []
+for j in range(n):
+   m.append(random.randint(1,n))
+print (m)
+
+file = open('D:\\Обучение\\Работа в Git\\Work_Python\\17\\file.txt', "w")
+for item in m: file.write('%s\n' % item) 
+file.close()
+
+temp = 0
+sum = 0
+file = open('D:\\Обучение\\Работа в Git\\Work_Python\\17\\file.txt', 'r')
+for j in file:
+   #for i in mas:
+      temp = j
+      print (temp)
+      sum = mas[j]      
+      print (sum)
+      #temp = mas(i)**2
+      #sum = sum + temp
+      #print (sum)
+
