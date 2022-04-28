496342265f14f7f52dd4dcd5e93382f818746519
Alex_T <saler@inbox.ru>
2022-04-28 22:05:47 +0300

    Задача 32

---
+++ Work_Python/32/32.py
@@ -0,0 +1,36 @@
+# 32.	Задайте последовательность чисел. Напишите программу, которая выведет 
+# список неповторяющихся элементов исходной последовательности.
+
+import os
+os.system('cls')
+import random
+
+n = int(input('Введите число =  '))
+
+mas = []
+for i in range(n*4):
+   mas.append(random.randint(0, 20))
+print('Это произвольный список =', mas)
+print()
+mas.sort()
+print('Это сортированный список =', mas)
+print()
+
+list = [1]
+list[0] = mas[0]
+
+x = int(list[0])
+temp = 0
+
+for i in range(len(mas)):
+    temp = int(mas[i])
+  
+    if x == temp:
+        x = temp
+        i = i + 1
+
+    if x < temp:
+        list.append(temp)
+        x = temp
+        i = i + 1
+print('Это неповторяющийся список',list)
