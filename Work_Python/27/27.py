a9d79447326bcc21e3b46eb3934cd4ba3bedf29b
Alex_T <saler@inbox.ru>
2022-05-05 17:43:23 +0300

    Задача 27

--- Work_Python/27/27.py
+++ Work_Python/27/27.py
@@ -1,2 +1,25 @@
 # Строка содержит набор чисел. Показать большее и меньшее число
 # Символ-разделитель - пробел
+
+import os
+import random
+os.system("cls")
+
+mas = []
+for i in range(19):
+   mas.append(random.randint(10,99))
+
+f = ""
+
+for i in mas:
+    f += str(i)+ " " 
+
+print('Строка из чисел через пробел = ', f)
+
+f1=list(f.split())
+mas1=list(map(int,f1))
+
+s1 = int(min(mas1))
+s2 = int(max(mas1))
+
+print ('Меньшее число =',s1,', Большее число = ',s2)
